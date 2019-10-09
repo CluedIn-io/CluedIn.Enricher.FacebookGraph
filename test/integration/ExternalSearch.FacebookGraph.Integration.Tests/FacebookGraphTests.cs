3e17b653237e680b4a461f886870cdb6f1befe78
Ben Fidge <ben.fidge@btopenworld.com>
2019-10-09 09:49:46 +0700

    Added integration tests

---
+++ test/integration/ExternalSearch.FacebookGraph.Integration.Tests/FacebookGraphTests.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FacebookGraphTests.cs" company="Clued In">
+//   Copyright (c) 2019 Clued In. All rights reserved.
+// </copyright>
+// <summary>
+//   Implements the facebook graph tests class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using CluedIn.Core.Data;
+using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Messages.Processing;
+using CluedIn.ExternalSearch.Providers.FacebookGraph;
+using CluedIn.Testing.Base.Context;
+using CluedIn.Testing.Base.ExternalSearch;
+using Moq;
+using Xunit;
+
+namespace ExternalSearch.FacebookGraph.Integration.Tests
+{
+    public class FacebookGraphTests : BaseExternalSearchTest<FacebookGraphExternalSearchProvider>
+    {
+        [Fact(Skip = "Skipped until app access token is able to make more requests pr hour")]
+        public void Test()
+        {
+            // App Token
+            var dummyTokenProvider = new DummyTokenProvider("1563070893926610|a0VBRWRXOkl4qx84YrTuMfhBTMA");
+            object[] parameters = { dummyTokenProvider };
+            
+            this.testContext = new TestContext();
+            IEntityMetadata entityMetadata = new EntityMetadataPart()
+                {
+                    Name        = "Sitecore",
+                    EntityType  = EntityType.Organization
+                };
+
+            this.Setup(parameters, entityMetadata);
+
+            // Assert
+            this.testContext.ProcessingHub.Verify(h => h.SendCommand(It.IsAny<ProcessClueCommand>()), Times.AtLeastOnce);
+
+            Assert.NotEmpty(this.clues);
+            Assert.NotNull(this.clues.First().Decompress().Data.EntityData.PreviewImage);
+        }
+    }
+}
---
+++ test/integration/ExternalSearch.FacebookGraph.Integration.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08907ddb-fdb1-4366-bb99-0a7a848d09c7")]
